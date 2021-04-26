1645961f2b036a8b50ade21e84b53e60cc3c5fba
sahasd <sahasd@gmail.com>
2021-04-26 04:03:10 -0400

    updated install to have petting zoo deps

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ python3 -m pip install atari-py --no-cache-dir
 python3 -m pip install tqdm --no-cache-dir
 python3 -m pip install plotly --no-cache-dir
 python3 -m pip install pettingzoo[all] --no-cache-dir 
+python3 -m pip install array2gif --no-cache-dir 
+python3 -m pip install supersuit --no-cache-dir 
