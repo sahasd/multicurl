0431706915ffcb1065075f4cb593c000e27c280b
sahasd <sahasd@gmail.com>
2021-04-26 01:05:13 -0400

    updated install to have petting zoo stuff

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ python3 -m pip install kornia --no-cache-dir
 python3 -m pip install atari-py --no-cache-dir
 python3 -m pip install tqdm --no-cache-dir
 python3 -m pip install plotly --no-cache-dir
+python3 -m pip install pettingzoo[all] --no-cache-dir 
